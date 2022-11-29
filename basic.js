7f3134d87d4dbddd6e14a0457e6ae1eae8a8f94d
Morning <morning@class3-3s-iMac.local>
2022-11-29 12:06:11 +0800

    edit

---
+++ basic.js
@@ -0,0 +1,138 @@
+// let a = 5;
+// console.log (a)
+// console.log (typeof(a));
+// let b = 5;
+// console.log(b)
+// console.log (typeof(b));
+
+
+// Daalgavar 
+
+// 1. 1. "Comments can make code readable" - нэг мөртэй коммент болгох 
+
+// let comment = "Comments can make code readable";
+
+// 2. "Javascript lesson begin" - нэг мөртэй коммент болгох 
+
+// let text = "Javascript lesson begin";
+// console.log (text);
+
+// 3. "Comments can make code readable" олон мөртэй коммент болгох 
+
+/* "Comments can make code readable" */
+
+// 4. Variable.js нэртэй файл үүсгэх, number, string, boolean, undefined, null зэрэг утгуудыг өгөх 
+
+let h = 9;
+let p = "9";
+let c = true;
+let d = false;
+let f;
+let g = null; 
+
+// 5. 5. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined,  null гэх мэт 
+
+let ar = 9;
+console.log (ar)
+console.log (typeof (ar));
+
+let pa = "9";
+console.log (pa)
+console.log (typeof (pa));
+
+let ca = true;
+console.log (ca)
+console.log (typeof (ca));
+
+let da = false;
+console.log (da)
+console.log (typeof (da));
+
+let fa ;
+console.log (fa)
+console.log (typeof (fa));
+
+let ga = null; 
+console.log (ga)
+console.log (typeof (ga));
+
+// 6. Утга оноогоогүй хувьсагч зарлах 
+
+let basic;
+console.log (basic);
+
+// 7. Утга оноосон хувьсагч зарлах 
+
+let test = 5;
+console.log (test);
+
+// 8. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+
+let FirstName;
+console.log (FirstName);
+
+let LastName;
+console.log(LastName);
+
+let MaritalStatus;
+console.log(MaritalStatus);
+
+let Country;
+console.log(Country);
+
+let Age;
+console.log(Age);
+
+// 9. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах 
+
+let FirstName1, LastName1, MaritalStatus1, Country1, Age1;
+console.log (FirstName1, LastName1, MaritalStatus1, Country1, Age1);
+
+// 10. MyAge, yourAge нэртэй хувьсагч зарлан эхний утгыг оноох 
+
+let MyAge = 1;
+console.log (MyAge);
+
+let yourAge = 2;
+console.log (yourAge);
+
+// 11. Гурвалжингийн периметрийг олох 
+let ss = 3;
+
+let hh = 4; 
+
+let gg = 5;                         
+
+sum = ss+hh+gg;
+console.log(sum);
+
+// 12. Тойргийн талбайг олох 
+
+let rr= 4;
+let yy= 2;
+
+sum= rr*2**2
+console.log(sum);
+
+// 13. m = y2-y1/x2-x1 хариуг олох 
+
+let y= 10;
+let x= 5;
+
+let div = y*2-y*1/x*2-x*1;
+
+console.log(div);
+
+// 14. 15 Gegabyte нь хэдэн биттэй тэнцүү вэ ?
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,6 @@
+let a = 9;
+let b = "9";
+let c = true;
+let d = false;
+let f;
+let g = null; 
